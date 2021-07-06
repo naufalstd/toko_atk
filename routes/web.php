5f9cbf34e599ef29109e761019a9ff5eebd44cca
Abdurrahman Naufal <abdurrahman.naufal9@gmail.com>
2021-07-06 16:58:51 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-
-Auth::routes();
-
-
 Route::group(['middleware' => 'auth'], function() {
 //USER
 
@@ -22,49 +18,51 @@ Route::group(['middleware' => 'auth'], function() {
 //     return view('dashboard');
 // });
 
-	Route::get('/', 'DashboardController@dashboard');
+//routing protecting
+
+Route::get('/', 'DashboardController@dashboard');
 
 
 
-	Route::get('hapus/{id}', 'PesanController@hapus')->name('hapus');
-	Route::get('barang', 'PesanController@barang');
+Route::get('hapus/{id}', 'PesanController@hapus')->name('hapus');
+Route::get('barang', 'PesanController@barang');
 
-	Route::get('categori', 'PesanController@barang');
-	Route::get('categori/{id}', 'PesanController@categori_getid');
+Route::get('categori', 'PesanController@barang');
+Route::get('categori/{id}', 'PesanController@categori_getid');
 
-	Route::get('keranjang/{id}', 'PesanController@categori_getid');
-	Route::get('keranjang', 'PesanController@keranjang');
-	Route::get('edit_keranjang/{id}', 'PesanController@edit_keranjang');
-	Route::post('update_keranjang/{id}', 'PesanController@update_keranjang');
+Route::get('keranjang/{id}', 'PesanController@categori_getid');
+Route::get('keranjang', 'PesanController@keranjang');
+Route::get('edit_keranjang/{id}', 'PesanController@edit_keranjang');
+Route::post('update_keranjang/{id}', 'PesanController@update_keranjang');
 
-	Route::get('pesanan', 'PesanController@pesanan');
-	Route::get('pesan/{id}', 'PesanController@index');
-	Route::post('pesan/{id}', 'PesanController@pesan');
+Route::get('pesanan', 'PesanController@pesanan');
+Route::get('pesan/{id}', 'PesanController@index');
+Route::post('pesan/{id}', 'PesanController@pesan');
 
-	Route::get('konfirmasi_selesai/{id}', 'PesanController@konfirmasi_selesai');
-	Route::get('konfirmasi_user', 'PesanController@konfirmasi_user');
-	Route::get('konfirmasi_atasan', 'PesanController@konfirmasi_atasan');
-
-
+Route::get('konfirmasi_selesai/{id}', 'PesanController@konfirmasi_selesai');
+Route::get('konfirmasi_user', 'PesanController@konfirmasi_user');
+Route::get('konfirmasi_atasan', 'PesanController@konfirmasi_atasan');
 
 
-	//ADMIN
-	Route::get('admin', 'AdminController@index');
-	Route::get('admin/detail/{id}', 'AdminController@detail');
-	Route::get('admin/konfirmasi_admin/{id}/{keterangan}', 'AdminController@konfirmasi_admin');
-	Route::get('admin/detail/edit', 'AdminController@edit_pesanan');
-	Route::post('admin/update_pesanan/{id}', 'AdminController@update_pesanan');
-	Route::post('admin/biaya', 'AdminController@biaya');
-	Route::get('admin/dana', 'AdminController@dana');
-	Route::post('admin/dana/update', 'AdminController@update_dana');
 
 
-	//ADMIN YANG BERKAITAN DENGAN BARANG
-	Route::get('admin/data', 'BarangController@index'); //tampilan utama 
-	Route::post('admin/data/store', 'BarangController@store'); //buat nambah data
-	Route::get('admin/data/edit/{id}', 'BarangController@edit'); //tampilan edit
-	Route::post('admin/data/update', 'BarangController@update'); //mengupdate database barang
-	Route::get('admin/data/delete/{id}', 'BarangController@delete'); //hapus barang
+//ADMIN
+Route::get('admin', 'AdminController@index');
+Route::get('admin/detail/{id}', 'AdminController@detail');
+Route::get('admin/konfirmasi_admin/{id}/{keterangan}', 'AdminController@konfirmasi_admin');
+Route::get('admin/detail/edit', 'AdminController@edit_pesanan');
+Route::post('admin/update_pesanan/{id}', 'AdminController@update_pesanan');
+Route::post('admin/biaya', 'AdminController@biaya');
+Route::get('admin/dana', 'AdminController@dana');
+Route::post('admin/dana/update', 'AdminController@update_dana');
+
+
+//ADMIN YANG BERKAITAN DENGAN BARANG
+Route::get('admin/data', 'BarangController@index'); //tampilan utama 
+Route::post('admin/data/store', 'BarangController@store'); //buat nambah data
+Route::get('admin/data/edit/{id}', 'BarangController@edit'); //tampilan edit
+Route::post('admin/data/update', 'BarangController@update'); //mengupdate database barang
+Route::get('admin/data/delete/{id}', 'BarangController@delete'); //hapus barang
 });
 
 Auth::routes();
